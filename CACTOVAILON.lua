acc2aa2bb98d40cf95a7bac362ae5e0729b8e38e
nhatminhcuto <nhatminhclonex2@gmail.com>
2025-05-05 12:50:22 +0800

    Update CACTOVAILON.lua

--- CACTOVAILON.lua
+++ CACTOVAILON.lua
@@ -1,11 +1,11 @@
 local placeId = game.PlaceId
 local scripts = {
-    [5902977746]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB.TD/Ultimate%20Tower%20Defense.lua",
-    [72829404259339]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB.TD/Anime%20Rangers%20X.lua",
-    [16480898254] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Eat%20The%20World.lua",
-    [116605585218149] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Go%20Fishing%20W1.lua",
-    [106962503558742] = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Go%20Fishing%20W2.lua",
-    [3101667897]  = "https://raw.githubusercontent.com/Minhdaynebro1/Dev-NhatMinh/refs/heads/main/WSJ_HUB/Legends%20Of%20Speed.lua"
+    [5902977746]  = "https://raw.githubusercontent.com/nhatminhcuto/CON-M-M-Y/refs/heads/main/N/M/U.T.D.lua",
+    [72829404259339]  = "https://raw.githubusercontent.com/nhatminhcuto/CON-M-M-Y/refs/heads/main/N/M/A.R.X.lua",
+    [16480898254] = "https://raw.githubusercontent.com/nhatminhcuto/CON-M-M-Y/refs/heads/main/N/M/E.T.W.lua",
+    [116605585218149] = "https://raw.githubusercontent.com/nhatminhcuto/CON-M-M-Y/refs/heads/main/N/M/G.F.W1.lua",
+    [106962503558742] = "https://raw.githubusercontent.com/nhatminhcuto/CON-M-M-Y/refs/heads/main/N/M/G.F.W2.lua",
+    [3101667897]  = ""
 }
 
 if scripts[placeId] then
